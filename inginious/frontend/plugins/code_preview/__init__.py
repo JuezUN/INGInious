d7baec781f0138cd8f0e7942947c3ba963a8ce52
Juan Valencia <judvalenciaan@unal.edu.co>
2019-10-06 08:36:16 -0500

    Changes in code_preview, code_review: pull_request: #135

--- inginious/frontend/plugins/code_preview/__init__.py
+++ inginious/frontend/plugins/code_preview/__init__.py
@@ -11,8 +11,15 @@ def init(plugin_manager, course_factory, client, config):
     plugin_manager.add_page('/api/code_preview/', TaskPreviewFileAPI)
     plugin_manager.add_page(r'/code_preview/static/(.*)', create_static_resource_page(_static_folder_path))
 
+    use_minified = config.get("use_minified", True)
+
+    if use_minified:
+        plugin_manager.add_hook("javascript_footer", lambda: "/code_preview/static/js/code_preview_load.min.js")
+    else:
+        plugin_manager.add_hook("javascript_footer", lambda: "/code_preview/static/js/code_preview_load.js")    
+
     plugin_manager.add_hook('task_editor_tab', code_preview_tab)
     plugin_manager.add_hook('task_editor_submit', on_task_editor_submit)    
-    plugin_manager.add_hook("javascript_footer", lambda: "/code_preview/static/js/code_preview_load.js")
+    
 
 
