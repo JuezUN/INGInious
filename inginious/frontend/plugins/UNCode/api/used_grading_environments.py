1e65445d73afadea79e9a095be306fe63d1c7737
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-10-16 19:19:30 -0500

    Fix in request

--- inginious/frontend/plugins/UNCode/api/used_grading_environments.py
+++ inginious/frontend/plugins/UNCode/api/used_grading_environments.py
@@ -1,7 +1,7 @@
-from inginious.frontend.plugins.utils.admin_api import AdminApi
 from ..constants import get_used_grading_environments
+import inginious.frontend.pages.api._api_page as api
 
 
-class UsedGradingEnvironments(AdminApi):
+class UsedGradingEnvironments(api.APIPage):
     def API_GET(self):
         return 200, get_used_grading_environments()
--- inginious/frontend/plugins/UNCode/api/used_subproblem_types.py
+++ inginious/frontend/plugins/UNCode/api/used_subproblem_types.py
@@ -1,7 +1,7 @@
-from inginious.frontend.plugins.utils.admin_api import AdminApi
+import inginious.frontend.pages.api._api_page as api
 from ..constants import get_used_subproblem_types
 
 
-class UsedSubproblemTypes(AdminApi):
+class UsedSubproblemTypes(api.APIPage):
     def API_GET(self):
         return 200, get_used_subproblem_types()
