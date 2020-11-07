6a59ad76cf0380632b03a9e92fe9d2f17409edd4
Cristian González <crdgonzalezca@unal.edu.co>
2020-11-07 09:38:19 -0500

    Change to data policy

--- inginious/frontend/plugins/UNCode/static/js/uncode.js
+++ inginious/frontend/plugins/UNCode/static/js/uncode.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function () {
                 '</a>' + ' - <a target="_blank" href="http://www.inginious.org" class="navbar-link">\n' +
                 'Powered by INGInious.\n</a>');
         footer.find('> div > div > div > div ').prepend('<p class="navbar-text">' +
-            '<a class="navbar-link" href="data_policy">Política de tratamiento de datos personales</a> </p>');
+            '<a class="navbar-link" href="data_policy">Data policy</a> </p>');
     }
 
     function updatePageIcon(imagePath) {
