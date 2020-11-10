4bb60020e2be95697acc2c90a052d202bf4ad1ca
Cristian González <crdgonzalezca@unal.edu.co>
2020-11-09 23:37:45 -0500

    Add pagination to course's tasks and mycourses (#237)
    * Add pagination to course's tasks
    
    * Add some comments
    
    * Do some refactoring
    
    * Some refactoring
    
    * Update translations
    
    * Paginate and add search to /mycourses
    
    * Some small fixes
    
    * Update translations
    
    * Fix import order
    
    * Fix search engine

--- inginious/frontend/pages/course.py
+++ inginious/frontend/pages/course.py
@@ -6,7 +6,7 @@
 """ Course page """
 import web
 
-from inginious.frontend.pages.utils import INGIniousAuthPage, INGIniousPage
+from inginious.frontend.pages.utils import INGIniousPage
 
 
 class CoursePage(INGIniousPage):
@@ -35,40 +35,69 @@ class CoursePage(INGIniousPage):
     def GET(self, courseid):  # pylint: disable=arguments-differ
         """ GET request """
         course = self.get_course(courseid)
-        return self.show_page(course)
+        user_input = web.input()
+        page = int(user_input.get("page", 1)) - 1
+        tag = user_input.get("tag", "")
+        return self.show_page(course, page, tag)
 
-    def show_page(self, course):
+    def show_page(self, course, current_page=0, current_tag=""):
         """ Prepares and shows the course page """
         username = self.user_manager.session_username()
         if not self.user_manager.course_is_open_to_user(course, lti=False):
             return self.template_helper.get_renderer().course_unavailable()
+
+        tasks = course.get_tasks()
+        last_submissions = self.submission_manager.get_user_last_submissions(5, {"courseid": course.get_id(),
+                                                                                 "taskid": {"$in": list(tasks.keys())}})
+
+        for submission in last_submissions:
+            submission["taskname"] = tasks[submission['taskid']].get_name(self.user_manager.session_language())
+
+        tasks_data = {}
+        user_tasks = self.database.user_tasks.find(
+            {"username": username, "courseid": course.get_id(), "taskid": {"$in": list(tasks.keys())}})
+        is_admin = self.user_manager.has_staff_rights_on_course(course, username)
+
+        tasks_score = [0.0, 0.0]
+
+        for taskid, task in tasks.items():
+            tasks_data[taskid] = {"visible": task.get_accessible_time().after_start() or is_admin, "succeeded": False,
+                                  "grade": 0.0}
+            tasks_score[1] += task.get_grading_weight() if tasks_data[taskid]["visible"] else 0
+
+        for user_task in user_tasks:
+            tasks_data[user_task["taskid"]]["succeeded"] = user_task["succeeded"]
+            tasks_data[user_task["taskid"]]["grade"] = user_task["grade"]
+
+            weighted_score = user_task["grade"] * tasks[user_task["taskid"]].get_grading_weight()
+            tasks_score[0] += weighted_score if tasks_data[user_task["taskid"]]["visible"] else 0
+
+        course_grade = round(tasks_score[0] / tasks_score[1]) if tasks_score[1] > 0 else 0
+        tag_list = course.get_all_tags_names_as_list(is_admin, self.user_manager.session_language())
+        user_info = self.database.users.find_one({"username": username})
+
+        # Filter tasks with the tag in case the tasks are filtered
+        if not current_tag:
+            filtered_tasks = tasks
         else:
-            tasks = course.get_tasks()
-            last_submissions = self.submission_manager.get_user_last_submissions(5, {"courseid": course.get_id(), "taskid": {"$in": list(tasks.keys())}})
+            filtered_tasks = {task_id: task for task_id, task in tasks.items() if
+                              current_tag in map(lambda x: x.get_name(), task.get_tags()[2] + task.get_tags()[0])}
 
-            for submission in last_submissions:
-                    submission["taskname"] = tasks[submission['taskid']].get_name(self.user_manager.session_language())
+        # Manage tasks pagination
+        page_limit = 20
+        total_tasks = len(filtered_tasks)
+        pages = total_tasks // page_limit
+        if (total_tasks % page_limit) != 0 or pages == 0:
+            pages += 1
+        if (page_limit * current_page + page_limit) < total_tasks:
+            page_tasks_ids = list(filtered_tasks.keys())[page_limit * current_page:
+                                                         page_limit * current_page + page_limit]
+        else:
+            page_tasks_ids = list(filtered_tasks.keys())[page_limit * current_page:]
 
-            tasks_data = {}
-            user_tasks = self.database.user_tasks.find({"username": username, "courseid": course.get_id(), "taskid": {"$in": list(tasks.keys())}})
-            is_admin = self.user_manager.has_staff_rights_on_course(course, username)
+        filtered_tasks = {task_id: tasks_data[task_id] for task_id, __ in filtered_tasks.items() if
+                          task_id in page_tasks_ids}
 
-            tasks_score = [0.0, 0.0]
-
-            for taskid, task in tasks.items():
-                tasks_data[taskid] = {"visible": task.get_accessible_time().after_start() or is_admin, "succeeded": False,
-                                      "grade": 0.0}
-                tasks_score[1] += task.get_grading_weight() if tasks_data[taskid]["visible"] else 0
-
-            for user_task in user_tasks:
-                tasks_data[user_task["taskid"]]["succeeded"] = user_task["succeeded"]
-                tasks_data[user_task["taskid"]]["grade"] = user_task["grade"]
-
-                weighted_score = user_task["grade"]*tasks[user_task["taskid"]].get_grading_weight()
-                tasks_score[0] += weighted_score if tasks_data[user_task["taskid"]]["visible"] else 0
-
-            course_grade = round(tasks_score[0]/tasks_score[1]) if tasks_score[1] > 0 else 0
-            tag_list = course.get_all_tags_names_as_list(is_admin, self.user_manager.session_language())
-            user_info = self.database.users.find_one({"username": username})
-
-            return self.template_helper.get_renderer().course(user_info, course, last_submissions, tasks, tasks_data, course_grade, tag_list)
+        return self.template_helper.get_renderer().course(user_info, course, last_submissions, tasks,
+                                                          filtered_tasks, course_grade, tag_list, pages,
+                                                          current_page + 1, current_tag)
--- inginious/frontend/pages/mycourses.py
+++ inginious/frontend/pages/mycourses.py
@@ -5,7 +5,8 @@
 
 """ Index page """
 from collections import OrderedDict
-
+import re
+import textdistance
 import web
 
 from inginious.frontend.pages.utils import INGIniousAuthPage
@@ -16,7 +17,10 @@ class MyCoursesPage(INGIniousAuthPage):
 
     def GET_AUTH(self):  # pylint: disable=arguments-differ
         """ Display main course list page """
-        return self.show_page(None)
+        user_input = web.input()
+        page = int(user_input.get("page", 1)) - 1
+        query = user_input.get("query", "")
+        return self.show_page(None, page, query)
 
     def POST_AUTH(self):  # pylint: disable=arguments-differ
         """ Parse course registration or course creation and display the course list page """
@@ -47,7 +51,39 @@ class MyCoursesPage(INGIniousAuthPage):
 
         return self.show_page(success)
 
-    def show_page(self, success):
+    def filter_courses(self, courses, query):
+        """
+        Filter the courses with respect the query. First, the course names and query are tokenized, removing
+        spaces, hyphens, and punctuation characters. Then, a score is obtained comparing each token of the
+        query with each course name, this is using the algorithm jaro-winkler, to calculate the distance
+        between two tokens. A list of tasks is obtained, and a task with a score of more or equal 0.9, is
+        appended. The resulting courses are sorted with respect the obtained score.
+         """
+        if query == "":
+            return courses
+
+        filtered_courses = []
+        for course in courses.values():
+            course_name = course.get_name(self.user_manager.session_language())
+            course_tokens = re.findall(r"[\w']+", course_name.lower())
+            query_tokens = re.findall(r"[\w']+", query.lower())
+            total_distance = 0
+            can_insert = False
+            for course_name_token in course_tokens:
+                for query_token in query_tokens:
+                    dist = textdistance.jaro_winkler(course_name_token, query_token)
+                    if dist >= 0.8:
+                        # Only distance values with >= 0.8 are taken into account to calculate the score
+                        # In case the distance is equal to 1, the weight is double for equal tokens.
+                        if dist == 1:
+                            dist *= 2
+                        total_distance += dist
+                        can_insert = True
+            if total_distance >= 0.9 and can_insert:
+                filtered_courses.append((course, total_distance))
+        return OrderedDict(map(lambda x: (x[0].get_id(), x[0]), sorted(filtered_courses, key=lambda x: -x[1])))
+
+    def show_page(self, success, current_page=0, query=""):
         """  Display main course list page """
         username = self.user_manager.session_username()
         user_info = self.database.users.find_one({"username": username})
@@ -58,8 +94,8 @@ class MyCoursesPage(INGIniousAuthPage):
         open_courses = {courseid: course for courseid, course in all_courses.items()
                         if self.user_manager.course_is_open_to_user(course, username, None) and
                         self.user_manager.course_is_user_registered(course, username)}
-        open_courses = OrderedDict(
-            sorted(iter(open_courses.items()), key=lambda x: x[1].get_name(self.user_manager.session_language())))
+        open_courses = OrderedDict(sorted(iter(open_courses.items()),
+                                          key=lambda x: x[1].get_name(self.user_manager.session_language())))
 
         last_submissions = self.submission_manager.get_user_last_submissions(5, {
             "courseid": {"$in": list(open_courses.keys())}})
@@ -78,5 +114,24 @@ class MyCoursesPage(INGIniousAuthPage):
         registerable_courses = OrderedDict(sorted(iter(registerable_courses.items()),
                                                   key=lambda x: x[1].get_name(self.user_manager.session_language())))
 
-        return self.template_helper.get_renderer().mycourses(open_courses, registerable_courses,
-                                                             except_free_last_submissions, success)
+        # Filter open courses
+        open_courses = self.filter_courses(open_courses, query)
+
+        # Manage courses pagination
+        page_limit = 20
+        total_open_courses = len(open_courses)
+        pages = total_open_courses // page_limit
+        if (total_open_courses % page_limit) != 0 or pages == 0:
+            pages += 1
+
+        if (page_limit * current_page + page_limit) < total_open_courses:
+            page_courses = list(open_courses.values())[page_limit * current_page:
+                                                       page_limit * current_page + page_limit]
+        else:
+            page_courses = list(open_courses.values())[page_limit * current_page:]
+
+        filtered_open_courses = OrderedDict(list(map(lambda x: (x.get_id(), x), page_courses)))
+
+        return self.template_helper.get_renderer().mycourses(filtered_open_courses, registerable_courses,
+                                                             except_free_last_submissions, success, pages,
+                                                             current_page + 1, query)
