8e84c8b7f4063f0a277543389883db0b8d3b80f1
AndresFelipeCastellanos <afcastellanosn@unal.edu.co>
2021-02-02 20:09:56 -0500

    Added some fixes

--- inginious/frontend/plugins/task_editorial/__init__.py
+++ inginious/frontend/plugins/task_editorial/__init__.py
@@ -12,10 +12,10 @@ def init(plugin_manager, course_factory, client , config):
     plugin_manager.add_page(r"/task_editorial/static/(.*)",create_static_resource_page(_TASK_EDITORIAL_STATIC_FOLDER_PATH_))
 
     if (use_minified):
+        plugin_manager.add_hook("javascript_footer", lambda: "/task_editorial/static/task_editorial.min.js")
+    else:
         plugin_manager.add_hook("javascript_footer", lambda: "/task_editorial/static/task_editorial.js")
         plugin_manager.add_hook("javascript_footer", lambda: "/task_editorial/static/task_editorial_preview.js")
-    #else:
-        #plugin_manager.add_hook("javascript_footer", lambda: "/task_editorial/static/task_editorial.min.js")
 
     plugin_manager.add_hook('task_editor_tab',editorial_task_tab)
     plugin_manager.add_hook('task_menu',editorial_task_preview)
--- inginious/frontend/plugins/task_editorial/pages/task_editorial.py
+++ inginious/frontend/plugins/task_editorial/pages/task_editorial.py
@@ -2,10 +2,11 @@ import os
 
 from inginious.frontend.plugins.multilang.problems.languages import get_all_available_languages
 from inginious.frontend.pages.course_admin.task_edit import CourseEditTask
+from inginious.frontend.parsable_text import ParsableText
 
 _TASK_EDITORIAL_TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "templates")
 
-def check_task_access(task):
+def is_task_open(task):
 
     return task.get_accessible_time().is_open()
 
@@ -15,19 +16,28 @@ def editorial_task_tab(course, taskid, task_data, template_helper):
     link = '<i class="fa fa-graduation-cap fa-fw"></i>&nbsp; ' + _("Task editorial")
 
     task_environment = task_data.get('environment')
+    task_solution_code_language = task_data.get('solution_code_language')
 
-    if(task_environment == "multiple_languages" or task_environment == "Data Science" or task_environment == "HDL" or task_environment is None):
-        content = template_helper.get_custom_renderer(_TASK_EDITORIAL_TEMPLATE_PATH, layout=False).task_editorial(task_data, get_all_available_languages())
+    if task_environment in {"multiple_languages" , "Data Science" , "HDL"} or task_environment is None:
+
+        content = template_helper.get_custom_renderer(_TASK_EDITORIAL_TEMPLATE_PATH, layout=False).task_editorial(task_data, get_all_available_languages(), task_solution_code_language)
         return tab_id, link ,content
     else:
         return
 
 def editorial_task_preview(course, task, template_helper):
 
-    if(check_task_access(task)):
+    if is_task_open(task):
         return
     else:
-        content = template_helper.get_custom_renderer(_TASK_EDITORIAL_TEMPLATE_PATH, layout=False).task_editorial_preview(course, task, get_all_available_languages())
+
+        task_tutorial_description_content = task._data.get('tutorial_description')
+        task_solution_code = task._data.get('solution_code')
+        task_solution_code_language = task._data.get('solution_code_language')
+
+        task_tutorial_description = ParsableText(task_tutorial_description_content, 'rst')
+
+        content = template_helper.get_custom_renderer(_TASK_EDITORIAL_TEMPLATE_PATH, layout=False).task_editorial_preview(course, task, get_all_available_languages(), task_tutorial_description, task_solution_code, task_solution_code_language)
         return str(content)
 
 def check_editorial_submit(course, taskid, task_data, task_fs):
