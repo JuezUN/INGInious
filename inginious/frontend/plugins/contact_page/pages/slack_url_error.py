264af0e5a00539b58e98dc0e8b59d85222754935
Edder <edhernandezfo@unal.edu.co>
2021-02-08 16:22:04 -0500

    improving code accord the lord bot wishes - css and slack error

--- inginious/frontend/plugins/contact_page/pages/slack_url_error.py
+++ inginious/frontend/plugins/contact_page/pages/slack_url_error.py
@@ -1,3 +1,4 @@
 class SlackURLError(ValueError):
+    """ Error getting main slack url """
     def __init__(self, message, *args):
-        super(SlackURLError, self).__init__(message, *args)
+        super().__init__(message, *args)
