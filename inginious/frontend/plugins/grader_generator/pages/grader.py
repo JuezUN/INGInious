6ccf95bfb9040d43206a25b269374e159bd0c9e1
Edder <edhernandezfo@unal.edu.co>
2021-04-22 17:07:29 -0500

    :construction:

--- inginious/frontend/plugins/grader_generator/pages/grader.py
+++ inginious/frontend/plugins/grader_generator/pages/grader.py
@@ -48,7 +48,7 @@ def grader_generator_tab(course, taskid, task_data, template_helper):
     link = '<i class="fa fa-check-circle fa-fw"></i>&nbsp; ' + _("Grader")
     task_data_list = task_data.get('grader_test_cases', [])
 
-    check_file_existence(course, taskid, task_data_list)
+    check_file_existence_for_multi_lang(course, taskid, task_data_list)
     grader_test_cases_dump = json.dumps(task_data_list)
     content = template_helper.get_custom_renderer(BASE_TEMPLATE_FOLDER, layout=False).grader(task_data,
                                                                                              grader_test_cases_dump,
@@ -67,18 +67,42 @@ def grader_generator_tab(course, taskid, task_data, template_helper):
     return tab_id, link, content
 
 
-def check_file_existence(course, task_id, task_data):
-    task_list = CourseTaskFiles.get_task_filelist(course._task_factory, course.get_id(), task_id)
+def check_file_existence_for_multi_lang(course, task_id, task_data):
+    environment_types_to_check = ["multiple_languages", "Data Science"]
+    environment = course.get_task(task_id).get_environment()
+    if environment not in environment_types_to_check:
+        # It only check for multi lang grader format
+        return
+
+    task_file_list = CourseTaskFiles.get_task_filelist(course._task_factory, course.get_id(), task_id).copy()
+
+    remove_test_without_file(task_file_list, task_data)
+
+
+def remove_test_without_file(task_file_list, task_data):
     file_full_name_index = 3
+    remove_public_files(task_file_list) # To reduce the number of comparations
     task_to_remove = []
     for task in task_data:
-        exist_input_file = any(file_name[file_full_name_index] == ("/" + task["input_file"]) for file_name in task_list)
+        exist_input_file = any(
+            file_name[file_full_name_index] == ("/" + task["input_file"]) for file_name in task_file_list)
         exist_output_file = any(
-            file_name[file_full_name_index] == ("/" + task["output_file"]) for file_name in task_list)
+            file_name[file_full_name_index] == ("/" + task["output_file"]) for file_name in task_file_list)
         if not (exist_output_file and exist_input_file):
             task_to_remove.append(task)
     for task in task_to_remove:
         task_data.remove(task)
+
+
+def remove_public_files(task_file_list):
+    task_path_to_remove = []
+    file_full_name_index = 3
+    substring = "/public/"
+    for path in task_file_list:
+        if path[file_full_name_index].find(substring) < 0:
+            task_path_to_remove.append(path)
+    for path_to_remove in task_path_to_remove:
+        task_file_list.remove(path_to_remove)
 
 
 def grader_footer(course, taskid, task_data, template_helper):
