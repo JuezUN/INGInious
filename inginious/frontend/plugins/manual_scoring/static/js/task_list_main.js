ff3e2b4a22c11c2238d0609931548a0c747c2b36
Cristian González <crdgonzalezca@unal.edu.co>
2021-07-09 11:46:45 -0500

    Allow course instructors to upload a custom rubric for manual grading (#346)
    * Add modal and initial changes for custom rubric
    
    * Add translations and remove manual scoring when the rubric is uploaded
    
    * Some refactoring
    
    * Mark message as translatable and translate
    
    * Fix on rubric uploading
    
    * Fix translation
    
    * Update readme
    
    * Remove print
    
    * Change alert-info style

--- inginious/frontend/plugins/manual_scoring/static/js/task_list_main.js
+++ inginious/frontend/plugins/manual_scoring/static/js/task_list_main.js
@@ -26,7 +26,6 @@ function exportCSVFile(items, fileTitle) {
     link.remove();
 }
 
-
 function addListenerToDownloadButton() {
     const course = getCourseId();
     $(`#${DOWNLOAD_CSV_BTN_ID}`).click(function () {
@@ -34,6 +33,106 @@ function addListenerToDownloadButton() {
     });
 }
 
+function displaySaveRubricAlertError(data) {
+    const alertElement = $("#save_rubric_alert");
+    alertElement.prop("class", "alert alert-danger");
+    if (typeof data !== "string") {
+        alertElement.text("Something went wrong while saving the rubric. Please try again.");
+    } else {
+        alertElement.text(data);
+    }
+    alertElement.prop("hidden", false);
+}
+
+function displaySaveRubricAlertSuccess(data) {
+    const alertElement = $("#save_rubric_alert");
+    alertElement.prop("class", "alert alert-success");
+    alertElement.text(data);
+    alertElement.prop("hidden", false);
+}
+
+function preventModalToBeClosed() {
+    $("#upload_custom_rubric_modal").modal({backdrop: "static", keyboard: false});
+    $("#upload_custom_rubric_modal button[data-dismiss=modal]").each(function () {
+        $(this).prop("disabled", true);
+    });
+}
+
+function makeModalClosable() {
+    $("#upload_custom_rubric_modal").modal({backdrop: '', keyboard: true});
+    $("#upload_custom_rubric_modal button[data-dismiss=modal]").each(function () {
+        $(this).prop("disabled", false);
+    });
+    $("#upload_custom_rubric_modal").scrollTop(0);
+}
+
+function onSubmitSaveRubric() {
+    $("form#upload_rubric_file").submit(function (e) {
+        e.preventDefault();
+        const file = $("#rubric_file").prop("files")[0];
+        const allowedFileExtensions = /(\.json)$/i;
+        if (file === undefined) {
+            displaySaveRubricAlertError("Please select a file before submitting it.");
+        } else if (!allowedFileExtensions.exec(file.name)) {
+            displaySaveRubricAlertError("The inserted file should be a .json file.");
+        } else {
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("course", getCourseId());
+            $.ajax({
+                url: "/api/manual_scoring/upload_custom_rubric",
+                method: "POST",
+                dataType: "json",
+                data: formData,
+                mimeType: "multipart/form-data",
+                processData: false,
+                contentType: false,
+                beforeSend: function () {
+                    $("#submit_save_rubric").prop("disabled", true);
+                    preventModalToBeClosed();
+                    alert("Do you want to upload this rubric?");
+                },
+                success: function (data) {
+                    makeModalClosable();
+                    if (data["status"] === "error") {
+                        displaySaveRubricAlertError(data["text"]);
+                    } else {
+                        displaySaveRubricAlertSuccess(data["text"]);
+                    }
+                    $("#submit_save_rubric").prop("disabled", false);
+                },
+                error: function (data) {
+                    displaySaveRubricAlertError(data["text"]);
+                    makeModalClosable();
+                    $("#submit_save_rubric").prop("disabled", false);
+                },
+                complete: () => {
+                    $("#rubric_file").val("");
+                }
+            });
+        }
+    });
+}
+
+function closeModalEvent() {
+    // Function to describe the process to follow when the modal is closed.
+    $("#upload_custom_rubric_modal").on("hidden.bs.modal", function () {
+        window.location.reload();
+    });
+}
+
+function setupDownloadRubric() {
+    const filename = "default_rubric.json";
+    const data = encodeURI("data:text/json;charset=utf-8," + getRubric());
+    const anchorElement = $("#download_rubric");
+
+    anchorElement.attr("href", data);
+    anchorElement.attr("download", filename);
+}
+
 jQuery(document).ready(function () {
-    addListenerToDownloadButton()
+    addListenerToDownloadButton();
+    onSubmitSaveRubric();
+    closeModalEvent();
+    setupDownloadRubric();
 });
