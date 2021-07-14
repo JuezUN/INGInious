13d1de0fd38c810ac940e7dbb1529af2d662319e
Cristian González <crdgonzalezca@unal.edu.co>
2021-07-14 18:17:50 -0500

    Load static files when the task page is loaded (#357)
    * Load static files when the task page is loaded
    
    * Fix the hook

--- inginious/frontend/pages/tasks.py
+++ inginious/frontend/pages/tasks.py
@@ -171,6 +171,8 @@ class BaseTaskPage(object):
             submissions = self.submission_manager.get_user_submissions(task) if self.user_manager.session_logged_in() else []
             user_info = self.database.users.find_one({"username": username})
 
+            self.template_helper.call("add_task_page_static_files", template_helper=self.template_helper)
+
             # Display the task itself
             return self.template_helper.get_renderer().task(user_info, course, task, submissions,
                                                             students, eval_submission, user_task, previous_taskid, next_taskid, self.webterm_link, random_input_list)
