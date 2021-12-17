9c3f5fc4898dbec4a7719107d43a71f55c1fe125
AndresFelipeCastellanos <afcastellanosn@unal.edu.co>
2021-12-16 19:05:27 -0500

    Added user registration checks and a new function to register student in course

--- inginious/frontend/plugins/lti_register/pages/lti_user_manager.py
+++ inginious/frontend/plugins/lti_register/pages/lti_user_manager.py
@@ -3,10 +3,14 @@ import web
 import hashlib
 import random
 import json
+from inginious.common.course_factory import InvalidNameException, CourseNotFoundException
+from inginious.frontend.pages.api._api_page import APIError
 from inginious.frontend.pages.utils import INGIniousPage 
 from inginious.frontend.plugins.utils import get_mandatory_parameter
+from inginious.frontend.plugins.register_students.pages.api.add_course_students_csv_file_api import random_password
 
 _LTI_REGISTRATION_TEMPLATES_PATH = "frontend/plugins/lti_register/pages/templates"
+_EMAIL_REGISTER_USER_TEMPLATES_PATH = "frontend/plugins/register_students/static"
 
 def get_user_lti(user_data):
 
@@ -15,7 +19,7 @@ def get_user_lti(user_data):
 
     user_username = user_email.split('@')[0]
 
-    lti_consumer = user_data["consumer_key"]
+    task = user_data["task"]
 
     new_user = {
         "username": user_username,
@@ -24,6 +28,7 @@ def get_user_lti(user_data):
         "language": "es",
         "ltibindings": {
         },
+        "task": task
     }
 
     return json.dumps(new_user)
@@ -46,43 +51,102 @@ class RegisterLTIPage(INGIniousPage):
     
     def POST(self):
 
-        data = get_mandatory_parameter(web.input(), "data")
+        new_user = get_mandatory_parameter(web.input(), "new_user")
+        data = json.loads(new_user)
 
-        course_id = data["courseid"]
-        course = self._get_course_and_check_rights(course_id)
+        (course_id, task_id)= data["task"]
 
-        if course is None:
-            return 200,{"status":"error","text":"The course does not exist."}
+        try:
+            course = self.course_factory.get_course(course_id); 
+        except (InvalidNameException, CourseNotFoundException):
+            raise APIError(400, {"error": _("The course does not exist.")})
         
-        if not data["realname"] or not data["username"] or not data["email"]:
+        if not data["realname"] or not data["username"] or not data["email"] or not course:
             return 200, {"status":"error","text":_("User cannot be registered. Some user data is no provided.")}
 
         user_data = {
             "username": data["username"] ,
             "realname": data["realname"],
-            "email": data["email"]
+            "email": data["email"],
+            "language": data["language"],
+            "ltibindings": data["ltibindings"],
+            "password": random_password(15).replace("{", "{{").replace("}", "}}")
         }
 
-        return self.register_user(course, user_data)
+        try:
+            success_user_registration_message = self.register_user(course, user_data)
+        except:
+            raise APIError(400, {"error": _("An error has occurred while registering the user.")})
+
+        return 200, success_user_registration_message
+        
 
     def register_user(self, course, user_data):
 
-        user = self.database.users.find_one({"$or":[{"username":user_data["username"]},
-                                                    {"email":user_data["email"]}
-                                                    ]})
-        if user is not None:
+        user_exists = self.already_user_exists(user_data["username"], user_data["email"])
 
-            if not self.user_manager.course_is_user_registered(course, user["username"]):
-                return 200, {"status":"error","text":_("User cannot be registered. The user is not registered in this course.")}
+        if user_exists:
+            return False
 
-        else:
-            self.database.uses.insert({
-                "username": user_data["username"],
-                "email": user_data["email"],
-                "realname": user_data["realname"],
-                "password": "passEje"
-            })
+        user_password_hash = hashlib.sha512(user_data["password"].encode("utf-8")).hexdigest()
+        user_activation_hash = hashlib.sha512(str(random.getrandbits(256)).encode("utf-8")).hexdigest()
 
-            return 200, {"status":"success","text":_("User was successfully registered.")}
+        user_data = {
+            "username": user_data["username"] ,
+            "realname": user_data["realname"],
+            "email": user_data["email"],
+            "language": user_data["language"],
+            "ltibindings": user_data["ltibindings"],
+            "password": user_password_hash,
+            "activate": user_activation_hash,
+            "bindings": {}
+        }
+
+        try:
+            user_activation_link = web.ctx.home + "/register?activate=" + user_activation_hash
+            data_policy_link = web.ctx.home + "/data_policy"
+            email_template = str(self.template_helper.get_custom_renderer(_EMAIL_REGISTER_USER_TEMPLATES_PATH).email_template()).format(
+                activation_link=user_activation_link, username=user_data["username"],
+                password=user_data["password"], course_name=course.get_name("en"), data_policy=data_policy_link)
+
+            self.database.users.insert(user_data)
+        except:
+            return json.dumps({"status": "error", "message": _("The new user was not created. Maybe this username or email are already taken")})
         
-        user_data = web.input()
+        is_user_registered_in_course  = self.register_in_course(course, user_data["username"])
+
+        if is_user_registered_in_course:
+
+            subject = _("Welcome on UNCode")
+            headers = {"Content-Type": 'text/html'}
+
+            try:
+                web.sendmail(web.config.smtp_sendername, user_data["email"], subject, email_template, headers)
+            except:
+                return json.dumps({"status": "error", "message": _("There was an error while sending the email")})
+
+        return json.dumps({"status":"succsess","text":_("Your UNCode account was successfully created!. You can now bind your new account.")})
+        
+    
+    def register_in_course(self, course, username):
+
+        user_registered_in_course = None
+
+        try:
+            user_registered_in_course = self.user_manager.course_register_user(course, username, '', True)
+        except:
+            pass
+
+        if user_registered_in_course:
+            return True
+        return False
+
+
+    def already_user_exists(self, username, email):
+        
+        user = self.database.users.find_one({"$or": [{"username":username},{"email":email}]})
+
+        if user:
+            return True
+
+        return False
--- inginious/frontend/plugins/lti_register/static/js/register_user_lti.js
+++ inginious/frontend/plugins/lti_register/static/js/register_user_lti.js
@@ -1,15 +1,30 @@
+function display_alert(){
+    let alert_id = "#registration_lti_alert";
+    let alert = $(alert_id);
+    alert.show();
+}
 
+function hide_alert(){
+    let alert_id = "#registration_lti_alert";
+    let alert = $(alert_id);
+    alert.hide();
+}
+
+function set_alert_message(message){
+    let alert_id = "#registration_lti_alert";
+    let alert = $(alert_id);
+    alert.text(message);
+}
 
 function register_user(){
     
     jQuery.ajax({
         method: "POST",
-        data: getUserDataLTI(),
-        success: () => {
-
+        data: {
+            "new_user": getUserDataLTI(),
         },
-        error: () => {
-
+        success: function(result){
+            console.log(result);
         }
     });
 
