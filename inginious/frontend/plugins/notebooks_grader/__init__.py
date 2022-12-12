1fef9d7f6db320ee4f2762ebf0488be7aeb4b094
Angel Gonzalez <angegonzalez@unal.edu.co>
2022-12-12 07:36:31 -0500

    Submission API refactor

--- inginious/frontend/plugins/notebooks_grader/__init__.py
+++ inginious/frontend/plugins/notebooks_grader/__init__.py
@@ -4,12 +4,22 @@
 # more information about the licensing of this file.
 """Notebooks grader serverless module"""
 
-from .api.notebook_grader_api import NotebookGradingAPI, TestNotebookSubmissionAPI, UserRolesAPI
+from .api.notebook_grader_api import NotebookGradingAPI, UserRolesAPI, notebook_submission
 
 
-def init(plugin_manager):
-    """ Init the plugin """
+def init(plugin_manager, _ , __, plugin_config):
+    """
+        Init the plugin.
+        Available configuration in configuration.yaml:
+        ::
+
+            - public_key_n: public key n value
+            - public_key_e: public key e value
+    """
+    public_key_n = int(plugin_config.get("public_key_n"))
+    public_key_e = int(plugin_config.get("public_key_e"))
+    public_key = (public_key_n, public_key_e)
     plugin_manager.add_page(r"/api/user_roles", UserRolesAPI)
     plugin_manager.add_page(r"/api/notebook_grader", NotebookGradingAPI)
     plugin_manager.add_page(
-        r"/api/notebook_grader/submit", TestNotebookSubmissionAPI)
+        r"/api/notebook_grader/submit", notebook_submission(public_key))
--- inginious/frontend/plugins/notebooks_grader/api/notebook_grader_api.py
+++ inginious/frontend/plugins/notebooks_grader/api/notebook_grader_api.py
@@ -4,12 +4,12 @@
 # more information about the licensing of this file.
 """Notebooks grader serverless module"""
 
-import inginious.frontend.pages.api._api_page as api
-from inginious.frontend.plugins.utils import get_mandatory_parameter
 from hashlib import sha512
 import datetime
-
 import web
+import inginious.frontend.pages.api._api_page as api
+from inginious.frontend.plugins.utils import get_mandatory_parameter
+from inginious.common.course_factory import CourseNotFoundException
 
 class NotebookGradingAPI(api.APIAuthenticatedPage):
     """API definition for get and set grader of a test"""
@@ -23,8 +23,8 @@ class NotebookGradingAPI(api.APIAuthenticatedPage):
 
         try:
             course = self.course_factory.get_course(course_id)
-        except:
-            raise api.APINotFound("Course not found")
+        except CourseNotFoundException as course_not_found:
+            raise api.APINotFound("Course not found") from course_not_found
 
         course_staff = course.get_staff()
         course_students = self.user_manager.get_course_registered_users(
@@ -56,8 +56,8 @@ class NotebookGradingAPI(api.APIAuthenticatedPage):
 
         try:
             course = self.course_factory.get_course(course_id)
-        except:
-            raise api.APINotFound("Course not found")
+        except CourseNotFoundException as course_not_found:
+            raise api.APINotFound("Course not found") from course_not_found
 
         course_staff = course.get_staff()
 
@@ -73,66 +73,71 @@ class NotebookGradingAPI(api.APIAuthenticatedPage):
                 "variables_names_to_evaluate": variables_names_to_evaluate,
             })
         else:
-            raise api.APIError(403,"You are not authorized to access this resource")
+            raise api.APIError(403, "You are not authorized to access this resource")
         return 200, "ok"
 
 
-class TestNotebookSubmissionAPI(api.APIAuthenticatedPage):
-    """API definition for do a task submission"""
-    
+def notebook_submission(public_key):
+    class TestNotebookSubmissionAPI(api.APIAuthenticatedPage):
+        """API definition for do a task submission"""
 
-    def API_POST(self): # pylint: disable=arguments-differ
-        username = self.user_manager.session_username()
-        request_params = web.input()
-        key_pair = (21145511420371257813590140607336605957103837517313322171470569706522561078664828667841325420528210362468905108841125312669175005069500260639399524062088068077504886974238207595257939432080569521137929147283808065912176990793482520433509116193089755149147387487723390391027270618541358331177840776511466141865612694492886915003526250148237699853388470861789974698225599828490908590893420408349423232934532594271550334201111542866305882684250049655689974023238891768700417170450261796942657506147044880425518280799814370090400325636947828503138490220047142351466235659660318214368333513255579788205983539615293773042251,65537)
-        course_id = get_mandatory_parameter(request_params, "course_id")
-        task_id = get_mandatory_parameter(request_params, "task_id")
-        test_id = get_mandatory_parameter(request_params, "test_id")
-        test_grade = get_mandatory_parameter(request_params, "test_grade")
-        result = get_mandatory_parameter(request_params, "result")
-        status = get_mandatory_parameter(request_params, "status")
-        test_grade_message = get_mandatory_parameter(
-            request_params, "test_grade_message")
-        functions_source_code = get_mandatory_parameter(
-            request_params, "functions_source_code")
-        variables_source_code = get_mandatory_parameter(
-            request_params, "variables_source_code")
-        signature = get_mandatory_parameter(
-            request_params, "signature")
-        msg = 'UNCode_notebook_grader.'+username+'.'+test_id
-        msg = str.encode(msg, encoding='utf-8')
-        hash_msg = int.from_bytes(sha512(msg).digest(), byteorder='big')
-        hash_from_signature = pow(int(signature), key_pair[1], key_pair[0])
+        def __validate_signature(self, username, test_id, signature):
+            msg = 'UNCode_notebook_grader.'+username+'.'+test_id
+            msg = str.encode(msg, encoding='utf-8')
+            hash_msg = int.from_bytes(sha512(msg).digest(), byteorder='big')
+            hash_from_signature = pow(int(signature), public_key[1], public_key[0])
 
-        if hash_msg != hash_from_signature:
-            raise api.APIError(502, "Signature is invalid")
+            if hash_msg != hash_from_signature:
+                raise api.APIError(502, "Signature is invalid")
 
-        try:
-            course = self.course_factory.get_course(course_id)
-        except:
-            raise api.APINotFound("Course not found")
 
-        course_staff = course.get_staff()
-        course_students = self.user_manager.get_course_registered_users(
-            course, with_admins=False)
+        def API_POST(self): # pylint: disable=arguments-differ
+            username = self.user_manager.session_username()
+            request_params = web.input()
+            course_id = get_mandatory_parameter(request_params, "course_id")
+            task_id = get_mandatory_parameter(request_params, "task_id")
+            test_id = get_mandatory_parameter(request_params, "test_id")
+            test_grade = get_mandatory_parameter(request_params, "test_grade")
+            result = get_mandatory_parameter(request_params, "result")
+            status = get_mandatory_parameter(request_params, "status")
+            test_grade_message = get_mandatory_parameter(
+                request_params, "test_grade_message")
+            functions_source_code = get_mandatory_parameter(
+                request_params, "functions_source_code")
+            variables_source_code = get_mandatory_parameter(
+                request_params, "variables_source_code")
+            signature = get_mandatory_parameter(
+                request_params, "signature")
 
-        if username in course_students and username not in course_staff:
-            self.database.submissions.insert({
-                "courseid": course_id,
-                "taskid": task_id,
-                "testid": test_id,
-                "grade": test_grade,
-                "result": result,
-                "status": status,
-                "test_grade_message": test_grade_message,
-                "functions_source_code": functions_source_code,
-                "variables_source_code": variables_source_code,
-                "submitted_on": datetime.datetime.utcnow(),
-                "username": [username]
-                })
-            return 200, "ok"
-        
-        raise api.APIError(403, "You are not authorized to access this resource")
+            self.__validate_signature(username, test_id, signature)
+            
+            try:
+                course = self.course_factory.get_course(course_id)
+            except CourseNotFoundException as course_not_found:
+                raise api.APINotFound("Course not found") from course_not_found
+
+            course_staff = course.get_staff()
+            course_students = self.user_manager.get_course_registered_users(
+                course, with_admins=False)
+
+            if username in course_students and username not in course_staff:
+                self.database.submissions.insert({
+                    "courseid": course_id,
+                    "taskid": task_id,
+                    "testid": test_id,
+                    "grade": test_grade,
+                    "result": result,
+                    "status": status,
+                    "test_grade_message": test_grade_message,
+                    "functions_source_code": functions_source_code,
+                    "variables_source_code": variables_source_code,
+                    "submitted_on": datetime.datetime.utcnow(),
+                    "username": [username]
+                    })
+                return 200, "ok"
+            raise api.APIError(403, "You are not authorized to access this resource")
+
+    return TestNotebookSubmissionAPI
 
 
 class UserRolesAPI(api.APIAuthenticatedPage):
@@ -142,7 +147,7 @@ class UserRolesAPI(api.APIAuthenticatedPage):
         course_id = get_mandatory_parameter(request_params, "course_id")
         try:
             course = self.course_factory.get_course(course_id)
-        except:
-            raise api.APINotFound("Course not found")
+        except CourseNotFoundException as course_not_found:
+            raise api.APINotFound("Course not found") from course_not_found
         roles = self.user_manager.user_roles(course)
         return 200, {"roles": roles}
