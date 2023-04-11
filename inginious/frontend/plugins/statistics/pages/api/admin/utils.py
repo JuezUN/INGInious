de660502140ce7bb3f43c0774e99936493836258
Juan Pablo Bustamante <jubustamantem@unal.edu.co>
2023-04-11 13:21:51 -0500

    fix best submissions details filter

--- inginious/frontend/plugins/statistics/pages/api/admin/utils.py
+++ inginious/frontend/plugins/statistics/pages/api/admin/utils.py
@@ -37,19 +37,18 @@ def project_detail_best_user_tasks(user_tasks, summary_result):
         "grade": s["grade"],
         "username": s["username"],
         "submission": project_best_submission(s.get("submission", None), summary_result)
-    } for s in user_tasks]
+    } for s in user_tasks if s.get("submission", None).get("custom", {}).get("custom_summary_result", "ACCEPTED" if s.get("submission", None).get("result",{}) == "success" else "WRONG_ANSWER") == summary_result]
 
 def project_best_submission(submission, summary_result):
     if submission is None:
         return None
-
-    if submission.get("custom", {}).get("custom_summary_result", "ACCEPTED" if submission["result"] == "success" else "WRONG_ANSWER") == summary_result:
-        return {
-            "id": str(submission["_id"]),
-            "submitted_on": submission["submitted_on"].isoformat(),
-            "taskId": submission["taskid"],
-            "status": submission["status"],
-            "result": submission["result"],
-            "grade": submission["grade"],
-            "summary_result": summary_result
-        }
+   
+    return {
+        "id": str(submission["_id"]),
+        "submitted_on": submission["submitted_on"].isoformat(),
+        "taskId": submission["taskid"],
+        "status": submission["status"],
+        "result": submission["result"],
+        "grade": submission["grade"],
+        "summary_result": summary_result
+    }
